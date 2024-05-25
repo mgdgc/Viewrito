6b9483621c3d33c279418d629e7bb4d80b4f9ffc
soc06212 <soc06212@gmail.com>
2024-05-26 01:08:29 +0900

    Deleted tests

--- Package.swift
+++ Package.swift
@@ -15,9 +15,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Viewrito"),
-        .testTarget(
-            name: "ViewritoTests",
-            dependencies: ["Viewrito"]),
+            name: "Viewrito")
     ]
 )
