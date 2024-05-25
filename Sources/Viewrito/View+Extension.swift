ec5c7beeac3494c2d6faa69fd8b6c6f960b1daa5
soc06212 <soc06212@gmail.com>
2024-05-26 00:53:47 +0900

    Add a view extension that does the same thing as Viewrito

--- Sources/Viewrito/View+Extension.swift
+++ Sources/Viewrito/View+Extension.swift
@@ -5,4 +5,11 @@
 //  Created by 최명근 on 5/26/24.
 //
 
-import Foundation
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+extension View {
+    func modifier<Modified>(_ modified: (Self) -> Modified) -> some View where Modified: View {
+        modified(self)
+    }
+}
