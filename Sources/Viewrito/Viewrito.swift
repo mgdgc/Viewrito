0ff1ee9639cd91d7cc3879f21dc19aabdaf471ca
soc06212 <soc06212@gmail.com>
2024-05-26 00:53:08 +0900

    Add a View that passes the received View directly to the closure

--- Sources/Viewrito/Viewrito.swift
+++ Sources/Viewrito/Viewrito.swift
@@ -1,2 +1,18 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Viewrito.swift
+//
+//
+//  Created by 최명근 on 5/20/24.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct Viewrito<Content, Returning>: View where Content: View, Returning: View {
+    let content: () -> Content
+    let returning: (Content) -> Returning
+    
+    public var body: some View {
+        returning(content())
+    }
+}
