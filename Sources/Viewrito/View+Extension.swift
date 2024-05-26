4929a62cc53c8abaf0f963557a626c9e111e679c
soc06212 <soc06212@gmail.com>
2024-05-26 15:05:06 +0900

    View extension fixed

--- Sources/Viewrito/View+Extension.swift
+++ Sources/Viewrito/View+Extension.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension View {
-    func modifier<Modified>(_ modified: (Self) -> Modified) -> some View where Modified: View {
+    public func addViewModifier<Modified>(@ViewBuilder modified: @escaping (Self) -> Modified) -> some View where Modified: View {
         modified(self)
     }
 }
